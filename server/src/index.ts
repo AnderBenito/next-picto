e02251dcd311c9d3b73670f529b279baf4c28c82
Ander Benito <anderbenito96@gmail.com>
2020-12-30 23:50:41 +0100

    Added basic room functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
+import { RoomMsg } from "../../shared/messages/room.message";
 import express from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
-import { ChatMsg } from "../../shared/messages/chat.messages";
-import { DrawMsg } from "../../shared/messages/draw.messages";
-import { DrawData } from "../../shared/models/draw.models";
-import { ChatData } from "./../../shared/models/chat.models";
+import { ChatMsg } from "../../shared/messages/chat.message";
+import { DrawMsg } from "../../shared/messages/draw.message";
+import { DrawData } from "../../shared/models/draw.model";
+import { ChatData } from "../../shared/models/chat.model";
+import { getUserById, joinUser } from "./utils/users";
+import { UserData } from "../../shared/models/user.model";
 
 const main = async () => {
 	const app = express();
@@ -17,32 +20,53 @@ const main = async () => {
 
 	io.on("connection", (socket: Socket) => {
 		console.log("New WS connection", socket.id);
+		socket.on(RoomMsg.join, (user: UserData) => {
+			user.userId = socket.id;
+			if (user && user.roomId) {
+				joinUser(user);
+				socket.join(user.roomId);
+				console.log("Joined to room", user.roomId);
+			}
+		});
+
+		socket.on(RoomMsg.leave, (user: UserData) => {
+			socket.leave(user.roomId);
+		});
+
 		socket.on(ChatMsg.message, (msg: ChatData) => {
-			console.log(msg);
-			socket.broadcast.emit("message", msg);
+			const user = getUserById(socket.id);
+			const newMsg = {
+				...msg,
+				username: user.username,
+			};
+			socket.broadcast.to(user.roomId).emit(ChatMsg.message, newMsg);
 		});
 
 		socket.on(DrawMsg.draw, (msg: DrawData) => {
-			console.log(msg);
-			socket.broadcast.emit(DrawMsg.draw, msg);
+			const user = getUserById(socket.id);
+
+			socket.broadcast.to(user.roomId).emit(DrawMsg.draw, msg);
 		});
 
 		socket.on(DrawMsg.draw_start, (msg: DrawData) => {
+			const user = getUserById(socket.id);
 			console.log("startdraw", msg);
-			socket.broadcast.emit(DrawMsg.draw_start, msg);
+			socket.broadcast.to(user.roomId).emit(DrawMsg.draw_start, msg);
 		});
 
 		socket.on(DrawMsg.draw_finish, () => {
+			const user = getUserById(socket.id);
 			console.log("finishdraw");
-			socket.broadcast.emit(DrawMsg.draw_finish);
+			socket.broadcast.to(user.roomId).emit(DrawMsg.draw_finish);
 		});
 
 		socket.on(DrawMsg.canvas_clear, () => {
+			const user = getUserById(socket.id);
 			console.log("clear_canvas");
-			socket.broadcast.emit(DrawMsg.canvas_clear);
+			socket.broadcast.to(user.roomId).emit(DrawMsg.canvas_clear);
 		});
 
-		socket.on("disconnect", (socket: Socket) => {
+		socket.on("disconnect", () => {
 			console.log("Closed connection with ", socket.id);
 		});
 	});
