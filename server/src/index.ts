8645f27a04ecaa8e7fadcee8f7455d5bc23caac9
Ander Benito <anderbenito96@gmail.com>
2021-01-01 13:57:56 +0100

    Separated cors config file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import corsConfig from "./config/corsConfig";
 import initSockets from "./utils/initSockets";
 
 const main = async () => {
 	const app = express();
 	const server = http.createServer(app);
 	const io = new Server(server, {
-		cors: {
-			origin: "http://localhost:3000",
-		},
+		cors: corsConfig,
 	});
 
 	initSockets(io);
