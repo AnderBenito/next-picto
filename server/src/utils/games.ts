42002e02c8e8d5a092acd0353eded197aa3fef38
Ander Benito <anderbenito96@gmail.com>
2021-01-06 00:11:44 +0100

    Added game and user functions

--- server/src/utils/games.ts
+++ server/src/utils/games.ts
@@ -1,30 +1,55 @@
 import { GameData } from "../../../shared/models/game.model";
 const games: GameData[] = [];
 
-export const createGame = (
-	roomId: string,
-	socketId: string,
-	totalTurns = 0,
-	finished = false
-) => {
+export const createGame = (roomId: string, userId: string, totalTurns = 0) => {
 	const newGameData: GameData = {
 		roomId,
-		host: socketId,
-		users: [socketId],
+		host: userId,
+		users: [],
 		currentTurn: 0,
 		totalTurns,
-		finished,
+		started: false,
+		finished: false,
 	};
 	games.push(newGameData);
 
 	return games;
 };
 
-export const deleteGame = (roomId: string, socketId: string) => {
+export const joinGame = (roomId: string, userId: string) => {
+	const index = games.findIndex((game) => game.roomId === roomId);
+	if (index === -1) return games;
+	if (games[index].users.includes(userId)) return games;
+
+	games[index].users.push(userId);
+
+	return games;
+};
+
+export const leaveGame = (roomId: string, userId: string) => {
+	const index = games.findIndex((game) => game.roomId === roomId);
+	if (index === -1) return games;
+	const userIndex = games[index].users.findIndex((user) => user === userId);
+	if (userIndex === -1) return games;
+	games[index].users.splice(userIndex, 1);
+
+	return games;
+};
+
+export const deleteGame = (roomId: string, userId: string) => {
 	const index = games.findIndex((game) => game.roomId === roomId);
 	if (index === -1) return;
-	if (games[index].host !== socketId) return;
+	if (games[index].host !== userId) return;
 	games.splice(index, 1);
 
 	return games;
+};
+
+export const startGame = (roomId: string, userId: string) => {
+	const index = games.findIndex((game) => game.roomId === roomId);
+	if (index === -1) return;
+	if (games[index].host !== userId) return;
+	games[index].started = true;
+
+	return games[index];
 };
--- server/src/utils/users.ts
+++ server/src/utils/users.ts
@@ -4,22 +4,24 @@ const users: ServerUserData[] = [];
 export const joinUser = (user: ServerUserData) => {
 	if (!users.find((u) => u.socketId === user.socketId)) {
 		users.push(user);
-		console.log("Total users: ", users);
 	}
+	return users;
 };
 
 export const leaveUser = (roomId: string) => {
 	const index = users.findIndex((u) => u.roomId === roomId);
 	if (index === -1) return;
 	users.splice(index, 1);
-	console.log(users);
+
+	return users;
 };
 
 export const removeUser = (socketId: string) => {
 	const index = users.findIndex((u) => u.socketId === socketId);
 	if (index === -1) return;
 	users.splice(index, 1);
-	console.log(users);
+
+	return users;
 };
 
 export const getUserById = (userId: string) => {
