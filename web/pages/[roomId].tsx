302d1d3beb1d309da91280013eb9d4d5e7f8af5c
Ander Benito <anderbenito96@gmail.com>
2021-01-06 00:13:42 +0100

    Update roomId when query changes

--- web/pages/[roomId].tsx
+++ web/pages/[roomId].tsx
@@ -1,28 +1,27 @@
 import { useRouter } from "next/router";
 import React, { useContext, useEffect } from "react";
-import DrawingBoardContainer from "../src/containers/DrawingBoardContainer";
+import GameContainer from "../src/containers/GameContainer";
+import { GameManagerProvider } from "../src/context/GameManagerProvider";
 import { SocketContext } from "../src/context/SocketProvider";
-import Game from "../styles/Game.module.css";
 
 const Example: React.FC = () => {
-	const { joinRoom, leaveRoom, deleteGame } = useContext(SocketContext);
+	const { setRoomId } = useContext(SocketContext);
 	const { query } = useRouter();
 
 	useEffect(() => {
 		const roomId = query.roomId as string;
 		if (!roomId) return;
-		joinRoom(roomId);
+		setRoomId(roomId);
 
 		return () => {
-			leaveRoom(roomId);
-			deleteGame(roomId);
+			setRoomId("");
 		};
 	}, [query]);
 
 	return (
-		<div className={Game.container}>
-			<DrawingBoardContainer />
-		</div>
+		<GameManagerProvider>
+			<GameContainer />
+		</GameManagerProvider>
 	);
 };
 
