f0e756a1275ddeb4e0fd80a5ddfee6d399ff6dc8
Ander Benito <anderbenito96@gmail.com>
2021-01-10 21:55:59 +0100

    Added aditional data

--- shared/models/game.model.ts
+++ shared/models/game.model.ts
@@ -2,6 +2,8 @@ export interface GameData {
 	roomId: string;
 	host: string;
 	users: string[];
+	turnOf: string;
+	turnIndex: number;
 	currentTurn: number;
 	totalTurns: number;
 	started: boolean;
