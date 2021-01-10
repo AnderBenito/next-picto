734738a3e7f416a3c1ef05ba190174484baefed5
Ander Benito <anderbenito96@gmail.com>
2021-01-10 01:29:18 +0100

    User removal on disconnection

--- server/src/sockets/initSockets.ts
+++ server/src/sockets/initSockets.ts
@@ -1,22 +1,29 @@
 import { Server, Socket } from "socket.io";
-import UserService from "../services/UserService";
-import room from "./room";
-import game from "./game";
 import draw from "./draw";
 import chat from "./chat";
+import game from "./game";
+import UserService from "../services/UserService";
+import GameService from "../services/GameService";
 
 export default (io: Server) => {
 	io.on("connection", (socket: Socket) => {
 		console.log("New WS connection", socket.id);
 
-		room(socket);
-		game(socket);
 		draw(socket);
 		chat(socket);
+		game(socket);
 
 		socket.on("disconnect", () => {
 			console.log("Closed connection with ", socket.id);
-			UserService.removeUser(socket.id);
+			UserService.removeUserBySocketId(socket.id).then((user) => {
+				console.log("Removing user", user);
+				GameService.leaveGame({
+					roomId: user.roomId,
+					userId: user.userId,
+					username: user.username,
+				});
+			});
+			// UserService.removeUser(socket.id);
 		});
 	});
 };
