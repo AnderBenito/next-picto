5483d8cde63b73f3833a85f23af8f8001fd4c6f0
Ander Benito <anderbenito96@gmail.com>
2021-01-14 20:31:44 +0100

    Update timer on tickTimer event from socket

--- web/src/containers/TimerContainer.tsx
+++ web/src/containers/TimerContainer.tsx
@@ -1,5 +1,9 @@
+import { time } from "console";
+import { create } from "domain";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { GameMsg } from "../../../shared/messages/game.message";
+import { TimerData } from "../../../shared/models/game.model";
+import { SocketData } from "../../../shared/models/socket.model";
 import Timer from "../components/Atoms/Timer";
 import { GameManagerContext } from "../context/GameManagerProvider";
 import { SocketContext } from "../context/SocketProvider";
@@ -13,15 +17,37 @@ const TimerContainer: React.FC = () => {
 	const [timer, setTimer] = useState<number>(TIMEOUT_TOTAL);
 	const intervalId = useRef<NodeJS.Timeout>();
 
+	//Do countdown on timer
 	const countDown = () => {
-		if (timer <= 0) {
-			console.log("TIME OUT");
-			clearInterval(intervalId.current);
-			setTimer(TIMEOUT_TOTAL);
-			socket.emit(GameMsg.finishTurn, createMessage(roomId));
+		if (timer > 0) {
+			const timerMsg: SocketData<TimerData> = {
+				userData: createMessage(roomId),
+				msgData: {
+					timerValue: timer,
+				},
+			};
+			socket.emit(GameMsg.tickTimer, timerMsg);
+			return;
 		}
+
+		clearInterval(intervalId.current);
+		setTimer(TIMEOUT_TOTAL);
+		socket.emit(GameMsg.finishTurn, createMessage(roomId));
 	};
 
+	//Set socket timer listeners on component mount
+	useEffect(() => {
+		socket.on(GameMsg.tickTimer, (timerMsg: SocketData<TimerData>) => {
+			if (!timerMsg || !timerMsg.msgData) return;
+			setTimer(timerMsg.msgData.timerValue);
+		});
+
+		return () => {
+			socket.off(GameMsg.tickTimer);
+		};
+	}, []);
+
+	//Set countdown interval when gameData changes
 	useEffect(() => {
 		if (!gameData || !isMyTurn()) return;
 
@@ -35,7 +61,9 @@ const TimerContainer: React.FC = () => {
 		};
 	}, [gameData]);
 
+	//Count down when timer changes
 	useEffect(() => {
+		if (!isMyTurn()) return;
 		countDown();
 	}, [timer]);
 
